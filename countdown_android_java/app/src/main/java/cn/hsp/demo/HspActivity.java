79103de75bdaf265403f61c93fd5469402d1a2e7
jerry <309032663@qq.com>
2021-12-11 17:56:20 +0800

    update

--- countdown_android_java/app/src/main/java/cn/hsp/demo/HspActivity.java
+++ countdown_android_java/app/src/main/java/cn/hsp/demo/HspActivity.java
@@ -1,8 +1,13 @@
 package cn.hsp.demo;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 /**
  * 厦门大学计算机专业 | 前华为工程师
  * 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
@@ -10,10 +15,32 @@ import android.os.Bundle;
  * 公众号：花生皮编程
  */
 public class HspActivity extends AppCompatActivity {
+    private final int interval = 50;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private TextView addTenSecondsTv;
+    private TextView addThirtySecondsTv;
+    private TextView addOneMinuteTv;
+    private CheckBox clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hsp);
+        addTenSecondsTv = findViewById(R.id.addTenSecondsTv);
+        addThirtySecondsTv = findViewById(R.id.addThirtySecondsTv);
+        addOneMinuteTv = findViewById(R.id.addOneMinuteTv);
+
+    }
+
+    private void startTimer() {
+        handler.postDelayed(this::changeBalls, interval);
+    }
+
+    private void stopTimer() {
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    private void changeBalls() {
+
     }
 }
