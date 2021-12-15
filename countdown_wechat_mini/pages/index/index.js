33dd1b2d61898ee6790dd3025f14e4e00ccc3c4d
jerry <309032663@qq.com>
2021-12-15 22:11:20 +0800

    update

--- countdown_wechat_mini/pages/index/index.js
+++ countdown_wechat_mini/pages/index/index.js
@@ -1,80 +1,143 @@
 //index.js
-//获取应用实例
-var app = getApp()
-var interval = null;
+var util = require('../../utils/util.js')
+
+let mmssArr = util.generateTimeArr(60);
+let hhArr = util.generateTimeArr(12);
+let interval = null;
 
 Page({
   data: {
+    hh: 0,
+    mm: 0,
+    ss: 0,
+    hhArr: hhArr,
+    mmArr: mmssArr,
+    ssArr: mmssArr,
+    timeInSecond: 0,
+    displayTime: '00:00:00',
     time: 0,
-    displayTime: '00:00:00'
-  },
-  
-  onStartHandler(){
-    if(!interval){
-      interval = setInterval(()=>{
-          this.setData({
-            time: this.data.time +1,
-            displayTime: this.parseTime(this.data.time)
-          })
-        }, 10);
-      }
+    screenStyle: 'container-countdown',
+    screenText: '倒计时',
+    alarmSrc: '../../assets/beep.mp3',
   },
 
-  parseTime(){
-    var mm = parseInt(this.data.time / 100/ 60);
-    if(mm<10) mm = '0'+mm;
-    var ss = parseInt(this.data.time % 6000 / 100);
-    if(ss<10) ss = '0'+ss;
-    var ssss = parseInt(this.data.time % 100);
-    if(ssss<10) ssss = '0'+ssss;
-    return `${mm}:${ss}:${ssss}`
+hhBindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      hh: e.detail.value
+    })
+  },
+
+mmBindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+        mm: e.detail.value
+    })
+},
+
+ssBindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+        ss: e.detail.value
+    })
+},
+
+  onStartHandler(){
+      if((!interval)&&(this.data.timeInSecond!=0)){
+          this.setData({screenStyle: 'container-countdown'});
+          this.setData({screenText: '倒计时'})
+          this.setData({
+                action: {
+                    method: 'pause'
+                }
+            })
+          interval = setInterval(()=>{
+          this.setData({
+              timeInSecond: this.data.timeInSecond-1
+          })
+          this.setData({
+                //timeInSecond: util.getTotalSeconds(this.data.hh, this.data.mm, this.data.ss),
+                displayTime: util.getHHSSMM(this.data.timeInSecond)
+            })
+        if(this.data.timeInSecond<=0){
+            clearInterval(interval);
+            interval = null;
+            this.setData({screenStyle: 'time-up-container'});
+            this.setData({screenText: '时间到!!!'})
+
+            this.setData({
+                action: {
+                    method: 'play'
+                }
+            })
+        }
+      }, 1000)
+      }else{
+          console.log('interval', interval)
+      }
+      
   },
 
   onStopHandler(){
-    console.log('stop')
-    if(interval){
-      clearInterval(interval);
-      interval = null;
-    }else{
+      this.setData({screenStyle: 'container-countdown'});
+      this.setData({screenText: '倒计时'})
       this.setData({
-        time: 0,
-        displayTime: '00:00:00'
-      })
-    }
-    
-  },
-  
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  },
-  onShow(){
-    if(!interval&&this.data.time!=0){
-      interval = setInterval(()=>{
-          this.setData({
-            time: this.data.time +1,
-            displayTime: this.parseTime(this.data.time)
-          })
-        }, 10);
+            action: {
+                method: 'pause'
+            }
+        })
+      if(interval){
+        clearInterval(interval);
+        interval = null;
+        this.setData({
+          timeInSecond: util.getTotalSeconds(this.data.hh, this.data.mm, this.data.ss),
+          //displayTime: util.getHHSSMM(this.data.timeInSecond)
+        });
+        this.setData({
+            //timeInSecond: util.getTotalSeconds(this.data.hh, this.data.mm, this.data.ss),
+            displayTime: util.getHHSSMM(this.data.timeInSecond)
+        })
       }
   },
-  onHide(){
-    console.log('onHide...')
-    if(interval){
-      clearInterval(interval);
-      interval = null;
-    }else{
+
+  onSetHandler(){
+      //console.log(this.data)
+      console.log(util.getHHSSMM(this.data.timeInSecond))
       this.setData({
-        time: 0,
-        displayTime: '00:00:00'
+          timeInSecond: util.getTotalSeconds(this.data.hh, this.data.mm, this.data.ss),
+          //displayTime: util.getHHSSMM(this.data.timeInSecond)
+      });
+      this.setData({
+          //timeInSecond: util.getTotalSeconds(this.data.hh, this.data.mm, this.data.ss),
+          displayTime: util.getHHSSMM(this.data.timeInSecond)
       })
-    }
+    console.log(this.data)
+  },
+
+  onHHChange(e){
+      var value = e.detail.value
+      if(!isNaN(value)){
+          this.setData({
+              hh: value
+          })
+      }
+  },
+
+  onMMChange(e){
+      var value = e.detail.value
+      if(!isNaN(value)){
+          this.setData({
+              mm: value
+          })
+      }
+  },
+
+  onSSChange(e){
+      var value = e.detail.value
+      if(!isNaN(value)){
+          this.setData({
+              ss: value
+          })
+      }
   }
 })
--- countdown_wechat_mini/utils/util.js
+++ countdown_wechat_mini/utils/util.js
@@ -1,19 +1,52 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-const formatNumber = n => {
+function formatNumber(n) {
   n = n.toString()
-  return n[1] ? n : `0${n}`
+  return n[1] ? n : '0' + n
+}
+
+function getTotalSeconds(hh, mm, ss){
+  return hh*3600 + mm*60 + ss*1
+}
+
+function getHHSSMM(totalInSecond){
+
+  var hh = parseInt(totalInSecond/3600)
+  if(hh<10) hh = '0'+hh;
+
+  var mm = parseInt(totalInSecond / 60 % 60 );
+  if(mm<10) mm = '0'+mm;
+
+  var ss = parseInt(totalInSecond % 60 % 60);
+  if(ss<10) ss = '0'+ss;
+  
+  return `${hh}:${mm}:${ss}`
+    
+}
+
+let generateTimeArr = (range) => {
+    //range should be the max value -1
+    let retArr = [];
+    for(let i=0; i<range; i++){
+      retArr[i] = i;
+    }
+    return retArr;
 }
 
 module.exports = {
-  formatTime
+  formatTime: formatTime,
+  getTotalSeconds: getTotalSeconds,
+  getHHSSMM: getHHSSMM,
+  generateTimeArr: generateTimeArr
 }
