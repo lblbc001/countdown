fd7e4a28f410fa507636acc737ffb60cced480f3
jerry <309032663@qq.com>
2024-10-05 21:04:46 +0800

    update

--- countdown_android_kotlin/app/src/main/java/cn/hsp/countdown/HspActivity.kt
+++ countdown_android_kotlin/app/src/main/java/cn/hsp/countdown/HspActivity.kt
@@ -1,3 +1,9 @@
+/**
+ * 厦门大学计算机专业 | 华为开发专家(HDE)
+ * 专注《零基础学编程系列》  http://lblbc.cn/blog
+ * 包含：鸿蒙 | Java | 安卓 | 前端 | Flutter | iOS | 小程序
+ * 公众号：蓝不蓝编程
+ */
 package cn.hsp.countdown
 
 import androidx.appcompat.app.AppCompatActivity
@@ -8,12 +14,6 @@ import android.os.Handler
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_hsp.*
 
-/**
- * 厦门大学计算机专业 | 前华为工程师
- * 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
- * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
- * 公众号：花生皮编程
- */
 class HspActivity : AppCompatActivity() {
     private val interval = 1000
     private val handler = Handler(Looper.getMainLooper())
